cb285298feb8fe5c3c49ef5ec367b0e5e6e070ed
sanjayk <sanjayk@qburst.com>
2021-02-20 01:09:17 +0530

    Adding Remaining Commits

--- todo/settings.py
+++ todo/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'testuser',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
